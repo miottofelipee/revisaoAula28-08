4bf7533b8a0bc176cbb68b567ff7b4e752443515
Fe Miotto <felipemiotto.564@gmail.com>
2024-08-28 12:29:29 -0300

    feat: screen Dino

--- src/routes/stack.routes.jsx
+++ src/routes/stack.routes.jsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screns/Home";
+import Dino from "../screns/Dino";
 
 const Stack = createNativeStackNavigator();
 
@@ -7,6 +8,7 @@ const StackRoutes = () => {
     return(
         <Stack.Navigator>
             <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="Dino" component={Dino}/>
         </Stack.Navigator>
     );
 };
---
+++ src/screns/Dino/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Dino() {
+  return (
+    <View>
+      <Text> Dino</Text>
+    </View>
+  )
+}
