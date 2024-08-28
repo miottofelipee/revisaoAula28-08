edb946fd59fe60802d1a804e8dbff8ff2ebbd93c
Fe Miotto <felipemiotto.564@gmail.com>
2024-08-28 12:57:18 -0300

    feat: create screns

--- src/screns/Dino/index.jsx
+++ src/screns/Dino/index.jsx
@@ -1,9 +1,16 @@
 import { View, Text } from 'react-native'
+import { TouchableOpacity } from 'react-native-web'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Dino() {
+
+  const navigation = useNavigation();
   return (
     <View>
-      <Text> Dino</Text>
+      <Text>Dino</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Text>Back to Home</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/screns/Home/index.jsx
+++ src/screns/Home/index.jsx
@@ -1,9 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, Touchable } from 'react-native'
+import { TouchableOpacity } from 'react-native-web'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Home() {
+
+  const navigation = useNavigation();
   return (
     <View>
       <Text>Home</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Dino')}>
+        <Text>Go to Dino</Text>
+      </TouchableOpacity>
     </View>
   )
 }
